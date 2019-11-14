e1e37e19828cdb4cfd79bbc4fde3a1aa5cd821ab
Wen Yu'an <405139216@qq.com>
2019-11-14 15:36:17 -0500

    update

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-$(document).ready(function() {
+$(document).ready(function () {
   // the tab click and scroll, maybe rewrite it to make it less repetitive
   var homepos = 0;
-  var workspos = $("#works").position().top -32;
-  $("#homeLink").click(function(event) {
+  var workspos = $("#works").position().top - 32;
+  $("#homeLink").click(function (event) {
     event.preventDefault();
     $("#main").animate({ scrollTop: homepos }, 300);
   });
-  $("#worksLink").click(function(event) {
+  $("#worksLink").click(function (event) {
     event.preventDefault();
     $("#main").animate({ scrollTop: workspos }, 300);
   });
-  
+
   // change tab bar css according scrollbar
 
 });
